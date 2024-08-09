e02d8cc17cbeb79e720bd8a38d1c015a86925043
Martin König <martin.koenig@ymail.com>
2024-08-09 12:46:26 +0200

    feat(data-intake,logs): add id column

--- rs/data-intake/src/middleware/greptime/insert.rs
+++ rs/data-intake/src/middleware/greptime/insert.rs
@@ -1,16 +1,22 @@
 use greptimedb_ingester::api::v1::{column, Column, ColumnDataType, InsertRequest, SemanticType};
 use shared::types::metadata::Metadata;
+use uuid::Uuid;
 
 use crate::process::logs::ParsedLine;
 
 pub fn to_insert_request(logs: Vec<ParsedLine>, metadata: &Metadata) -> InsertRequest {
     let rows = logs.len();
 
-    let (ts, text) = logs.into_iter().fold(
-        (Vec::with_capacity(rows), Vec::with_capacity(rows)),
+    let (ts, text, id) = logs.into_iter().fold(
+        (
+            Vec::with_capacity(rows),
+            Vec::with_capacity(rows),
+            Vec::with_capacity(rows),
+        ),
         |mut acc, log| {
             acc.0.push(log.dt.timestamp_millis());
             acc.1.push(log.text);
+            acc.2.push(Uuid::new_v4().to_string()); // id column with UUID
             acc
         },
     );
@@ -32,6 +38,17 @@ pub fn to_insert_request(logs: Vec<ParsedLine>, metadata: &Metadata) -> InsertRe
             column_name: "text".to_owned(),
             values: Some(column::Values {
                 string_values: text.into_iter().collect(),
+                ..Default::default()
+            }),
+            semantic_type: SemanticType::Field as i32,
+            datatype: ColumnDataType::String as i32,
+            ..Default::default()
+        },
+        // id column
+        Column {
+            column_name: "id".to_owned(),
+            values: Some(column::Values {
+                string_values: id,
                 ..Default::default()
             }),
             semantic_type: SemanticType::Field as i32,
