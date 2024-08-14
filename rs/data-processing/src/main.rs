979769c7f64b5f4e06c142ac0e1d834b7cb962ae
Martin König <martin.koenig@ymail.com>
2024-08-14 14:49:53 +0200

    refactor(data-processing): add create consumer function

--- rs/data-processing/src/main.rs
+++ rs/data-processing/src/main.rs
@@ -1,21 +1,35 @@
+use fluvio::dataplane::link::ErrorCode;
+use fluvio::dataplane::record::ConsumerRecord;
 use fluvio::{
     consumer::{ConsumerConfigExtBuilder, OffsetManagementStrategy},
     Offset,
 };
-use futures_util::StreamExt;
+use futures_util::{Stream, StreamExt};
 use shared::{
     connections::fluvio::connect::{FluvioConnection, DEFAULT_TOPIC},
     tracing::setup::setup_tracing,
 };
-use tracing::{error, info};
+use tracing::error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_tracing();
 
+    let mut consumer = create_consumer().await?;
+
+    while let Some(Ok(record)) = consumer.next().await {
+        println!("{}", String::from_utf8_lossy(record.as_ref()));
+    }
+
+    Ok(())
+}
+
+async fn create_consumer(
+) -> Result<impl Stream<Item = Result<ConsumerRecord, ErrorCode>> + Unpin, Box<dyn std::error::Error>>
+{
     let fluvio = FluvioConnection::new().await.unwrap();
 
-    let mut consumer = fluvio
+    let consumer = fluvio
         .fluvio
         .consumer_with_config(
             ConsumerConfigExtBuilder::default()
@@ -37,9 +51,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .unwrap();
 
-    while let Some(Ok(record)) = consumer.next().await {
-        println!("{}", String::from_utf8_lossy(record.as_ref()));
-    }
-
-    Ok(())
+    Ok(consumer)
 }
