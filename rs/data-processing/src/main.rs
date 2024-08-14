64742cb3c8c9ceb4f4c99b30883e4869ca2bb532
Martin König <martin.koenig@ymail.com>
2024-08-14 14:59:42 +0200

    feat(data-processing): add consumer error

--- rs/data-processing/src/main.rs
+++ rs/data-processing/src/main.rs
@@ -5,14 +5,27 @@ use fluvio::{
     Offset,
 };
 use futures_util::{Stream, StreamExt};
+use shared::connections::fluvio::connect::ConnectionError;
 use shared::{
     connections::fluvio::connect::{FluvioConnection, DEFAULT_TOPIC},
     tracing::setup::setup_tracing,
 };
+use thiserror::Error;
 use tracing::error;
 
+#[derive(Error, Debug)]
+pub enum ConsumerError {
+    #[error("Fluvio connection error: {0}")]
+    FluvioConnectionError(#[from] ConnectionError),
+    #[error("Consumer config error: {0}")]
+    ConsumerConfigError(String),
+    #[error("Consumer error: {0}")]
+    ConsumerError(String),
+    #[error("Other error: {0}")]
+    Other(#[from] Box<dyn std::error::Error>),
+}
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), ConsumerError> {
     setup_tracing();
 
     let mut consumer = create_consumer().await?;
@@ -25,11 +38,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn create_consumer(
-) -> Result<impl Stream<Item = Result<ConsumerRecord, ErrorCode>> + Unpin, Box<dyn std::error::Error>>
-{
-    let fluvio = FluvioConnection::new().await.unwrap();
+) -> Result<impl Stream<Item = Result<ConsumerRecord, ErrorCode>> + Unpin, ConsumerError> {
+    let fluvio_connection = FluvioConnection::new().await?;
 
-    let consumer = fluvio
+    let consumer = fluvio_connection
         .fluvio
         .consumer_with_config(
             ConsumerConfigExtBuilder::default()
@@ -40,16 +52,14 @@ async fn create_consumer(
                 .build()
                 .map_err(|e| {
                     error!("Error creating consumer config: {}", e);
-                    e
-                })
-                .unwrap(),
+                    ConsumerError::ConsumerConfigError(e.to_string())
+                })?,
         )
         .await
         .map_err(|e| {
             error!("Error creating consumer: {}", e);
-            e
-        })
-        .unwrap();
+            ConsumerError::ConsumerError(e.to_string())
+        })?;
 
     Ok(consumer)
 }
