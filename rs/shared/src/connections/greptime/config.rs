e0ea608113a210d7cadce72e06d69d0f8ae9e4cd
Martin König <martin.koenig@ymail.com>
2024-08-22 18:21:44 +0200

    feat(shared,greptime): add custom errors

--- rs/shared/src/connections/greptime/config.rs
+++ rs/shared/src/connections/greptime/config.rs
@@ -1,7 +1,14 @@
 const DEFAULT_GREPTIME_RPC_PORT: &str = "4001";
 const DEFAULT_GREPTIME_PSQL_PORT: &str = "4003";
 
-use std::env::var;
+use std::env::{var, VarError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GreptimeConfigError {
+    #[error("Environment variable {0}, error: {1}")]
+    EnvVarError(String, #[source] VarError),
+}
 
 #[derive(Clone)]
 pub struct GreptimeConfig {
@@ -11,23 +18,24 @@ pub struct GreptimeConfig {
     pub db_name: String,
 }
 impl GreptimeConfig {
-    pub fn new() -> Result<Self, String> {
+    pub fn new() -> Result<Self, GreptimeConfigError> {
         Ok(Self {
             host: var("GREPTIMEDB_HOST")
-                .map_err(|e| format!("{e}: GREPTIMEDB_HOST: provide localhost or FQDN"))?,
+                .map_err(|e| GreptimeConfigError::EnvVarError("GREPTIMEDB_HOST".to_owned(), e))?,
             port: DEFAULT_GREPTIME_RPC_PORT.to_owned(),
             psql_port: DEFAULT_GREPTIME_PSQL_PORT.to_owned(),
-            db_name: var("DB_NAME").map_err(|e| format!("{e}: DB_NAME"))?,
+            db_name: var("DB_NAME")
+                .map_err(|e| GreptimeConfigError::EnvVarError("DB_NAME".to_owned(), e))?,
         })
     }
     pub fn get_uri(&self) -> String {
         let GreptimeConfig { host, port, .. } = self;
         format!("{host}:{port}")
     }
-    pub fn get_psql_uri(&self) -> String {
+    pub fn get_psql_uri(&self, db_name: &str) -> String {
         let GreptimeConfig {
             host, psql_port, ..
         } = self;
-        format!("postgresql://{host}:{psql_port}")
+        format!("postgresql://{host}:{psql_port}/{db_name}")
     }
 }
