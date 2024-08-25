34963d0d71cd67b1f9c14103dbda1afad1b12fae
Martin König <martin.koenig@ymail.com>
2024-08-25 08:33:35 +0200

    feat(shared,tracing): add line numbers

--- rs/shared/src/tracing/setup.rs
+++ rs/shared/src/tracing/setup.rs
@@ -1,15 +1,18 @@
 use std::{env::var, sync::Once};
+use tracing_subscriber::{fmt, EnvFilter, FmtSubscriber};
 
-use tracing_subscriber::{EnvFilter, FmtSubscriber};
-
-static INIT: Once = Once::new();
+static TRACING_INIT: Once = Once::new();
 
 pub fn setup_tracing() {
-    // call_once allows to initialize the logger from multiple tests running in separated threads
-    INIT.call_once(|| {
+    // ensures that the subscriber is only initialized once for all threads
+    TRACING_INIT.call_once(|| {
         let filter = EnvFilter::new(var("RUST_LOG").unwrap_or_else(|_| String::from("info")));
 
-        let subscriber = FmtSubscriber::builder().with_env_filter(filter).finish();
+        let subscriber = FmtSubscriber::builder()
+            .with_env_filter(filter)
+            .fmt_fields(fmt::format::DefaultFields::new())
+            .event_format(fmt::format().compact().with_line_number(true))
+            .finish();
 
         tracing::subscriber::set_global_default(subscriber)
             .expect("setting default subscriber failed");
