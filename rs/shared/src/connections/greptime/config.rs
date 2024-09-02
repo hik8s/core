4a2b19cfff66df91f5786db357220894601c6d80
Martin König <martin.koenig@ymail.com>
2024-09-02 12:41:18 +0200

    refactor(shared,greptime): add default db name

--- rs/shared/src/connections/greptime/config.rs
+++ rs/shared/src/connections/greptime/config.rs
@@ -3,7 +3,7 @@ const DEFAULT_GREPTIME_PSQL_PORT: &str = "4003";
 
 use std::env::var;
 
-use crate::connections::shared::error::ConfigError;
+use crate::{connections::shared::error::ConfigError, constant::GREPTIME_DB_NAME};
 
 #[derive(Clone)]
 pub struct GreptimeConfig {
@@ -19,8 +19,7 @@ impl GreptimeConfig {
                 .map_err(|e| ConfigError::EnvVarError(e, "GREPTIMEDB_HOST".to_owned()))?,
             port: DEFAULT_GREPTIME_RPC_PORT.to_owned(),
             psql_port: DEFAULT_GREPTIME_PSQL_PORT.to_owned(),
-            db_name: var("DB_NAME")
-                .map_err(|e| ConfigError::EnvVarError(e, "DB_NAME".to_owned()))?,
+            db_name: GREPTIME_DB_NAME.to_owned(),
         })
     }
     pub fn get_uri(&self) -> String {
--- rs/shared/src/constant.rs
+++ rs/shared/src/constant.rs
@@ -1,5 +1,12 @@
+// chat-backend
+// pub const OPENAI_CHAT_MODEL: &str = "gpt-3.5-turbo-0125";
+pub const OPENAI_CHAT_MODEL: &str = "gpt-4-0125-preview";
+
 // qdrant
 pub const QDRANT_COLLECTION_LOG: &str = "logs";
+
+// greptime
+pub const GREPTIME_DB_NAME: &str = "logs";
 
 // embedding
 pub const OPENAI_EMBEDDING_MODEL: &str = "text-embedding-3-large";
