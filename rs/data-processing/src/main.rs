1c736b02073c4609eae88d7900877f86bab0c3e5
“Martin <martin.koenig@ymail.com>
2024-10-14 16:56:19 +0200

    refactor(data-processing): add run fn

--- rs/data-processing/src/main.rs
+++ rs/data-processing/src/main.rs
@@ -1,3 +1,4 @@
+use shared::constant::TOPIC_LOG_PARTITIONS;
 use shared::fluvio::{FluvioConnectionError, TopicName};
 use shared::{fluvio::FluvioConnection, tracing::setup::setup_tracing};
 use thiserror::Error;
@@ -17,11 +18,10 @@ pub enum DataProcessingError {
     ProcessThreadError(#[from] ProcessThreadError),
 }
 
-#[tokio::main]
-async fn main() -> Result<(), DataProcessingError> {
-    setup_tracing();
+pub async fn run_data_processing() -> Result<(), DataProcessingError> {
     let fluvio_logs = FluvioConnection::new(TopicName::Log).await?;
     let fluvio_class = FluvioConnection::new(TopicName::Class).await?;
+
     // Vector to hold all spawned threads
     let mut threads: Vec<JoinHandle<Result<(), DataProcessingError>>> = Vec::new();
 
@@ -40,5 +40,12 @@ async fn main() -> Result<(), DataProcessingError> {
         thread.await??;
     }
 
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), DataProcessingError> {
+    setup_tracing();
+    run_data_processing().await?;
     Ok(())
 }
