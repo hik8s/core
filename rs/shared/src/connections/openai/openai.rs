d9dbcefdc8833402fc4103880743e257b5d62585
“Martin <martin.koenig@ymail.com>
2025-01-09 16:37:56 +0100

    feat(chat-backend): add resource spec retrieval

--- rs/shared/src/connections/openai/openai.rs
+++ rs/shared/src/connections/openai/openai.rs
@@ -64,19 +64,26 @@ impl OpenAIConnection {
         // the args are not required for ChatCompletionTool
         let log_retrieval: ChatCompletionTool =
             Tool::LogRetrieval(LogRetrievalArgs::default()).into();
+        // let cluster_overview = Tool::ClusterOverview.into();
         let event_retrieval: ChatCompletionTool =
             Tool::EventRetrieval(EventRetrievalArgs::default()).into();
         let resource_status_retrieval: ChatCompletionTool =
             Tool::ResourceStatusRetrieval(ResourceStatusRetrievalArgs::default()).into();
         let customresource_status_retrieval: ChatCompletionTool =
             Tool::CustomResourceStatusRetrieval(ResourceStatusRetrievalArgs::default()).into();
+        let resource_spec_retrieval: ChatCompletionTool =
+            Tool::ResourceSpecRetrieval(ResourceStatusRetrievalArgs::default()).into();
+        let customresource_spec_retrieval: ChatCompletionTool =
+            Tool::CustomResourceSpecRetrieval(ResourceStatusRetrievalArgs::default()).into();
 
         let tools = vec![
             log_retrieval,
-            Tool::ClusterOverview.into(),
+            // cluster_overview,
             event_retrieval,
             resource_status_retrieval,
             customresource_status_retrieval,
+            resource_spec_retrieval,
+            customresource_spec_retrieval,
         ];
         self.request_builder(messages, model, 1024, Some(num_choices), None, Some(tools))
     }
