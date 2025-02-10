e97ce67fb0484af59ad0465fe5aad7ec14609aa3
“Martin <martin.koenig@ymail.com>
2025-02-10 13:57:12 +0100

    refactor(tests,util): move uid replace fn

--- rs/tests/src/integration_test/data_intake_processing.rs
+++ rs/tests/src/integration_test/data_intake_processing.rs
@@ -30,7 +30,6 @@ mod tests {
     use std::sync::{Arc, Mutex, Once};
     use std::time::{Duration, Instant};
     use tracing::info;
-    use uuid7::uuid4;
 
     use tokio::time::sleep;
 
@@ -40,7 +39,7 @@ mod tests {
         Update,
     }
 
-    use crate::util::read_yaml_files;
+    use crate::util::{read_yaml_files, replace_resource_uids};
 
     static THREAD_LOG_PROCESSING: Once = Once::new();
 
@@ -126,58 +125,6 @@ mod tests {
         assert_eq!(rows.len(), test_data.raw_messages.len());
         assert_eq!(classes.len(), test_data.expected_class.count as usize);
         Ok(())
-    }
-
-    fn replace_resource_uids(resources: &mut [serde_json::Value], db: &DbName) -> String {
-        let resource_uid = uuid4().to_string();
-
-        let mut owner_uids_map: HashMap<String, String> = HashMap::new();
-
-        for resource in resources.iter_mut() {
-            if let Some(json) = resource.get_mut("json") {
-                let key = match db {
-                    DbName::Resource => "metadata",
-                    DbName::CustomResource => "metadata",
-                    DbName::Event => "involvedObject",
-                    _ => "",
-                };
-                if let Some(metadata) = json.as_object_mut().and_then(|obj| obj.get_mut(key)) {
-                    if let Some(metadata_obj) = metadata.as_object_mut() {
-                        metadata_obj.insert(
-                            "uid".to_string(),
-                            serde_json::Value::String(resource_uid.clone()),
-                        );
-                        if db == &DbName::Resource {
-                            if let Some(owner_refs) = metadata_obj.get_mut("ownerReferences") {
-                                if let Some(owner_refs_array) = owner_refs.as_array_mut() {
-                                    for owner_ref in owner_refs_array {
-                                        if let Some(owner_ref_obj) = owner_ref.as_object_mut() {
-                                            let original_owner_uid = owner_ref_obj
-                                                .get("uid")
-                                                .and_then(|v| v.as_str())
-                                                .unwrap_or("")
-                                                .to_string();
-
-                                            let owner_uid = owner_uids_map
-                                                .entry(original_owner_uid)
-                                                .or_insert_with(|| uuid4().to_string())
-                                                .clone();
-
-                                            owner_ref_obj.insert(
-                                                "uid".to_string(),
-                                                serde_json::Value::String(owner_uid),
-                                            );
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        resource_uid
     }
 
     static THREAD_RESOURCE_PROCESSING: Once = Once::new();
