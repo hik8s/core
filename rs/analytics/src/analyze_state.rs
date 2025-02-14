6b717a9c98955332dc49c5cb81b36dd7e6609c38
“Martin <martin.koenig@ymail.com>
2025-02-14 15:32:58 +0100

    chore(analytics,state): match by name

--- rs/analytics/src/analyze_state.rs
+++ rs/analytics/src/analyze_state.rs
@@ -1,16 +1,17 @@
 use redis::Commands;
 
-use shared::connections::redis::connect::RedisConnection;
+use shared::connections::{dbname::DbName, redis::connect::RedisConnection};
 
 pub async fn analyze_state(redis: &mut RedisConnection, customer_id: &str) {
-    let match_key = format!("{customer_id}:kube-apiserver*");
+    let match_key = format!("{}_*", DbName::Log.id(customer_id));
     let keys: Vec<String> = redis.connection.keys(&match_key).unwrap();
-    println!("Found {} keys in Redis:", keys.len());
+    tracing::info!(
+        "Found {} keys in Redis that match: {}",
+        keys.len(),
+        match_key
+    );
     for key in keys {
-        let _pod_name = key.split(":").last().unwrap();
-
-        // let state = redis.get(customer_id, pod_name).unwrap();
-
-        // println!("{pod_name} state len: {}", state.classes.len());
+        let state = redis.get(&key).unwrap();
+        tracing::info!("{key} state len: {}", state.classes.len());
     }
 }
