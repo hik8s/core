724c9b370eb63a704c885373f93aab3e645ce314
“Martin <martin.koenig@ymail.com>
2025-02-16 14:33:30 +0100

    refactor(shared,openai-connection): rename module

--- rs/shared/src/connections/mod.rs
+++ rs/shared/src/connections/mod.rs
@@ -9,5 +9,5 @@ mod util;
 
 pub use crate::connections::error::ConfigError;
 
-pub use crate::connections::openai::openai::OpenAIConnection;
+pub use crate::connections::openai::openai_connection::OpenAIConnection;
 pub use crate::connections::util::{get_env_var, get_env_var_as_vec};
