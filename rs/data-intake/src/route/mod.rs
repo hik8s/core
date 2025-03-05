1e9a2bbbd5edd6fd26825de01c1e4d7bdb175746
“Martin <martin.koenig@ymail.com>
2025-03-05 16:03:35 +0100

    refactor(data-intake): add intake to route file names

--- rs/data-intake/src/route/mod.rs
+++ rs/data-intake/src/route/mod.rs
@@ -1,9 +1,9 @@
-mod customresource;
-mod event;
-mod log;
-mod resource;
+mod intake_customresource;
+mod intake_event;
+mod intake_log;
+mod intake_resource;
 
-pub use customresource::{customresource_intake, customresources_intake};
-pub use event::{event_intake, events_intake};
-pub use log::log_intake;
-pub use resource::{resource_intake, resources_intake};
+pub use intake_customresource::{customresource_intake, customresources_intake};
+pub use intake_event::{event_intake, events_intake};
+pub use intake_log::log_intake;
+pub use intake_resource::{resource_intake, resources_intake};
