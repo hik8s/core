49d186ebf48bcfa02a62cb164c86e7d3114ba9f8
“Martin <martin.koenig@ymail.com>
2025-03-06 13:25:19 +0100

    refactor(tests,e2e): rename e2e resource integration

--- rs/tests/src/integration_test/mod.rs
+++ rs/tests/src/integration_test/mod.rs
@@ -1,5 +1,5 @@
-pub mod data_intake_processing;
 pub mod e2e_event_integration;
 pub mod e2e_log_integration;
+pub mod e2e_resource_integration;
 pub mod integrate_data_retrieval;
 pub mod prompt_openai_qdrant;
