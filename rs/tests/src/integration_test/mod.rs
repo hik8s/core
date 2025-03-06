43f5daaf0fcd35f0dc4bf32bd155b189e9266bac
“Martin <martin.koenig@ymail.com>
2025-03-06 13:18:14 +0100

    fix(tests): remove unused tests

--- rs/tests/src/integration_test/mod.rs
+++ rs/tests/src/integration_test/mod.rs
@@ -1,4 +1,5 @@
 pub mod data_intake_processing;
-pub mod data_intake_resource;
+pub mod e2e_event_integration;
+pub mod e2e_log_integration;
 pub mod integrate_data_retrieval;
 pub mod prompt_openai_qdrant;
