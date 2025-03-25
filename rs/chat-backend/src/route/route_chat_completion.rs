0f9abaa8763bcb4cc7f23c77445c5ef419f98795
“Martin <martin.koenig@ymail.com>
2025-03-25 13:05:31 +0100

    refactor(chat-backend): move request options to route

--- rs/chat-backend/src/route/route_chat_completion.rs
+++ rs/chat-backend/src/route/route_chat_completion.rs
@@ -4,12 +4,14 @@ use rocket::post;
 use rocket::response::stream::{Event, EventStream};
 use rocket::serde::json::Json;
 
+use shared::constant::OPENAI_CHAT_MODEL_MINI;
+use shared::openai_util::{create_assistant_message, create_system_message, create_user_message};
 use shared::{GreptimeConnection, QdrantConnection};
 use tokio;
 use tokio::sync::mpsc;
 use tracing::error;
 
-use crate::handle::handle_chat_completion::{process_user_message, RequestOptions};
+use crate::handle::handle_chat_completion::process_user_message;
 
 use super::header::LastEventId;
 
@@ -45,4 +47,52 @@ pub fn chat_completion(
             yield event;
         }
     }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+pub struct RequestOptions {
+    pub messages: Vec<Message>,
+    pub model: String,
+    pub client_id: String,
+    pub temperature: Option<f32>,
+    pub top_p: Option<f32>,
+}
+impl RequestOptions {
+    pub fn test(input: &str, client_id: &str) -> Self {
+        RequestOptions {
+            messages: vec![
+                Message {
+                    role: "system".to_string(),
+                    content: "not used".to_string(),
+                },
+                Message {
+                    role: "user".to_string(),
+                    content: input.to_string(),
+                },
+            ],
+            model: OPENAI_CHAT_MODEL_MINI.to_string(),
+            client_id: client_id.to_owned(),
+            temperature: None,
+            top_p: None,
+        }
+    }
+}
+impl From<RequestOptions> for Vec<ChatCompletionRequestMessage> {
+    fn from(val: RequestOptions) -> Self {
+        val.messages
+            .into_iter()
+            .map(|message| match message.role.as_str() {
+                "system" => create_system_message(),
+                "user" => create_user_message(&message.content),
+                "assistant" => create_assistant_message(&message.content, None),
+                _ => panic!("Unknown role in message: {message:?}"),
+            })
+            .collect()
+    }
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+pub struct Message {
+    pub role: String,
+    pub content: String,
 }
